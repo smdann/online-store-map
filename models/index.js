c382f0a2f68b4a1e5d25b8b75e3129f5fa287d46
SDann <smdann17@gmail.com>
2022-10-22 10:49:55 -0500

    Added association methods for the models in order to establish the relationships between them.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
-// import models
+// Import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Category has many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  }
+});
 
 module.exports = {
   Product,
